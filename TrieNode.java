7a0668427925ee248d12839d0b09dbf7ee6c083d
Sharanya8207 <bhumasharanya28@gmail.com>
2026-01-11 11:30:36 +0530

    Create TrieNode.java

---
+++ TrieNode.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TrieNode {
+    Map<Character, TrieNode> children;
+    boolean isEndOfWord;
+    int frequency;
+
+    public TrieNode() {
+        children = new HashMap<>();
+        isEndOfWord = false;
+        frequency = 0;
+    }
+}
